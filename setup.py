506cf890c8694e154ff105d425607a9c6c8f9dc0
Virantha Ekanayake <virantha@gmail.com>
2019-03-01 13:08:28 -0500

    fix to NAME being different now between package and path

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ with io.open(os.path.join(here, 'HISTORY.rst'), encoding='utf-8') as f:
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
-with open(os.path.join(here, NAME, '__version__.py')) as f:
+with open(os.path.join(here, 'bleak', '__version__.py')) as f:
     exec(f.read(), about)
 
 
